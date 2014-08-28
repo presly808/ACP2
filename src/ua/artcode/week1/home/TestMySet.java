0e435c6ee937e640969812351136a07eba780aa8
presly808 <presly808@gmail.com>
2014-08-29 00:35:50 +0300

    Set Iterator example test task luxsoft

---
+++ src/ua/artcode/week1/home/TestMySet.java
@@ -0,0 +1,26 @@
+package ua.artcode.week1.home;
+
+/**
+ * Created by admin on 27.08.2014.
+ */
+public class TestMySet {
+    public static void main(String[] args) {
+
+        MySet<String> stringMySet = new MySet<String>();
+        stringMySet.add("Serhii");
+        stringMySet.add("Valentin");
+        stringMySet.add("Oleg");
+        stringMySet.add("Viktor");
+        stringMySet.add("Sasha");
+        stringMySet.add("Masha");
+        stringMySet.add("Vika");
+        stringMySet.add("Evgen");
+
+
+        for(String s : stringMySet){
+            System.out.println(s);
+        }
+
+
+    }
+}
