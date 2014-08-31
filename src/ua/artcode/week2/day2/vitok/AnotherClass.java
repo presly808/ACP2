9b8d6d2e046a094f76cdb2311aa7dbdac5caf961
Viktoria Goncharuk <vitokg@gmail.com>
2014-08-31 16:55:17 +0300

    Another new class

---
+++ src/ua/artcode/week2/day2/vitok/AnotherClass.java
@@ -0,0 +1,10 @@
+package ua.artcode.week2.day2.vitok;
+
+/**
+ * Created by Vitok on 8/31/2014.
+ */
+public class AnotherClass {
+    public static void main(String[] args) {
+        System.out.println("hij");
+    }
+}
