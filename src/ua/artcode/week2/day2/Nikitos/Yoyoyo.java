38aaec29ac8687ac80da6a538cc991cac40c4e2f
gnrnrnrn <ggnrnlawyer@mail.ru>
2014-08-31 18:13:58 +0300

    info

---
+++ src/ua/artcode/week2/day2/Nikitos/Yoyoyo.java
@@ -0,0 +1,10 @@
+package ua.artcode.week2.day2.Nikitos;
+
+/**
+ * Created by 1 on 31.08.14.
+ */
+public class Yoyoyo {
+    public static void main(String[] args) {
+        System.out.println("Nigga!");
+    }
+}
