3e64ed715d313b5f6e62507a109f677642075e3d
Venikeee <Venik@i.ua>
2014-08-31 16:58:31 +0300

    sale

---
+++ src/ua/artcode/week2/day2/Venik/TestGit.java
@@ -0,0 +1,15 @@
+package ua.artcode.week2.day2.Venik;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by little on 31.08.2014.
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("продам ВАЗ 2109");
+        System.out.println("Bow wow");
+    }
+
+}
