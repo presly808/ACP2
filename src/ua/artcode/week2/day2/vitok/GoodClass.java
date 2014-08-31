dc2d5e30cc6a6a21c9fa54bf0e4c6ea5db55c7ea
Viktoria Goncharuk <vitokg@gmail.com>
2014-08-31 16:05:47 +0300

    Created new class for test

---
+++ src/ua/artcode/week2/day2/vitok/GoodClass.java
@@ -0,0 +1,11 @@
+package ua.artcode.week2.day2.vitok;
+
+/**
+ * Created by Vitok on 8/31/2014.
+ */
+public class GoodClass {
+
+    public static void main(String[] args) {
+        System.out.println("Good Morning!");
+    }
+}
