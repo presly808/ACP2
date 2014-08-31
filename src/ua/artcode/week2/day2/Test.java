6e70fb2b5ac8d7cf099d0617b8bdc8f1c0f0a7f4
Serhiy <dotsenko@i.ua>
2014-08-31 16:56:27 +0300

    first commit

--- src/ua/artcode/week2/day2/Test.java
+++ src/ua/artcode/week2/day2/Test.java
@@ -6,5 +6,6 @@ package ua.artcode.week2.day2;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hi everyone from theJobber");
     }
 }
