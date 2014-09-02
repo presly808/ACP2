38b2a21045622f0745522f52d246cf2a66fdd12d
SKolomiets <serhiy.kolomiets@gmail.com>
2014-09-02 13:38:23 +0300

    SKolomiets commit.

--- src/Test.java
+++ src/Test.java
@@ -5,35 +5,8 @@ import java.util.Date;
  */
 
 public class Test {
-    //Ctrl + Shift + F10 - Run main
-    //psvm - main method
-    //format code  Ctrl + Alt + L
     public static void main(String[] args) {
-        //sout  + TAb
-
-        System.out.println("Start main thread");
-        Date date = new Date();
-        long pointStart = date.getTime();
-        System.out.println(recFact(15));
-        long pointEnd = System.currentTimeMillis();
-        System.out.println("After recursion");
-        System.out.println("Time of work in millis = " + (pointEnd - pointStart));
-
-    }
-
-    // ternary
-    /*
-    * num < 2 ? 1 : (recFact(num - 1));
-    *
-    * if(num < 2){
-    *    return 1;
-    * } else {
-    *    return recFact(num - 1);
-    * }
-    *
-    * */
-    public static int recFact(int num) {
-        return num < 2 ? 1 : (num * recFact(num - 1));
+        System.out.println("Git test.");
     }
 
 
