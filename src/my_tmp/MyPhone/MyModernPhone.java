a7af9e53dd4de66c558111ad1f1237ce869665a9
presly808 <presly808@gmail.com>
2014-09-07 18:22:36 +0300

    fix

--- src/my_tmp/MyPhone/MyModernPhone.java
+++
@@ -1,9 +0,0 @@
-package my_tmp.MyPhone;
-
-/**
- * Created by Admin on 06.09.14.
- */
-public interface MyModernPhone {
-    void superCall();
-    void redirect();
-}
--- src/my_tmp/MyTest/MyArrayList.java
+++
@@ -1,7 +0,0 @@
-package my_tmp.MyTest;
-
-/**
- * Created by Admin on 06.09.14.
- */
-public class MyArrayList {
-}
