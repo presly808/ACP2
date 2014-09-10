6eb7673f029a0b5d78f2182fe73c63d064208e2e
yachmenev <yachmenevev@gmail.com>
2014-09-10 14:45:21 +0300

    added MyScanner class

---
+++ src/my_tmp/MyScanner/MyScannerTest.java
@@ -0,0 +1,13 @@
+package my_tmp.MyScanner;
+
+/**
+ * Created by Admin on 09.09.14.
+ */
+public class MyScannerTest {
+    public static void main(String[] args) {
+
+        MyScanner scanner = new MyScanner();
+        System.out.println(scanner.nextInt());
+        //System.out.println(scanner.next());
+    }
+}
