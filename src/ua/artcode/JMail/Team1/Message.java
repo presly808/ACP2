da77ca9ffbf93c96264b65c4e669890865debb4f
melnychukvv <meln_v@ukr.net>
2014-09-27 18:06:24 +0300

    Tree Rotation

---
+++ src/ua/artcode/JMail/Team1/Message.java
@@ -0,0 +1,7 @@
+package ua.artcode.JMail.Team1;
+
+/**
+ * Created by bm13 on 27.09.2014.
+ */
+public class Message {
+}
---
+++ src/ua/artcode/JMail/Team1/MessageInterface.java
@@ -0,0 +1,8 @@
+package ua.artcode.JMail.Team1;
+
+/**
+ * Created by bm13 on 27.09.2014.
+ */
+public class MessageInterface {
+    read();
+}
