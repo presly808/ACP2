9433fb77c13eef2d3eca49f7552e5571ef49ab32
presly808 <presly808@gmail.com>
2014-10-25 21:43:38 +0300

    web simple servlet api

---
+++ WebSimple/src/main/java/servlets/HelloServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class HelloServlet extends HttpServlet {
+
+    public HelloServlet() {
+        System.out.println("Create HELLO SERVLET");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> enumeration = req.getHeaderNames();
+        System.out.println("HEADERS");
+        while(enumeration.hasMoreElements()){
+            String key = enumeration.nextElement();
+            System.out.println(key + "=" + req.getHeader(key));
+        }
+
+        // PARAMS req.getParam*
+
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("");
+        pw.print("<h1>FIRST SERVLET</h1>");
+        pw.close();
+    }
+}
